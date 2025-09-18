a611f0d807ea5f697d0b341e8b2e4272f90093ed
monishkanigam <monishkanigam50@gmail.com>
2025-09-18 14:18:36 +0530

    authentication completed

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,44 +1,62 @@
 import { pool } from "../config/db/db.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { ZodError } from "zod";
+import { userCreationValidation, userLoginValidation } from "../../validation/zodValidation.js";
 
 
-
+// register 
 export const register = async (req, res) => {
     try {
-        const { username, email, password } = req.body;
-        if (!username || !email || !password) {
-            res.status(400).json({
-                message: "all fields are required"
-            })
-        }
-        const userExist = await pool.query("select * from users where email =$1", [email]);
+
+        // zod validation
+        const { username, email, password } = userCreationValidation.parse(req.body);
+
+
+        // check user exist
+        const userExist = await pool.query("select * from users where email=$1", [email]);
         if (userExist.rows.length > 0) {
             return res.status(400).json({
-                message: "user exist already"
+                message: "user already exists"
             })
         }
-        // hassed password
+
+
+        // hashed password and salt rounds=10
         const hashedPassword = await bcrypt.hash(password, 10);
 
+
         // inser new user
-        await pool.query(
-            "insert into users (username,email,password)values($1,$2,$3)", [username, email, hashedPassword]
+        const newUser = await pool.query(
+            "insert into users (username,email,password)values($1,$2,$3) returning *", [username, email, hashedPassword]
         );
+
+
+        // generate token for register
+        const token = jwt.sign({
+            id: newUser.rows[0].id
+        }, process.env.JWT_SECRET, { expiresIn: "1h" })
         res.status(201).json({
-            message: "user register successfully"
-        })
+            message: "user register successfully",
+            user: newUser.rows[0],
+            token,
+        });
     } catch (error) {
-        res.status(500).json({
-            error: error.message
-        })
+        if (error instanceof ZodError) {
+            return res.status(400).json({
+                message: "validation failed",
+                errors: error.errors
+            })
+        }
+        res.status(500).json({ error: error.message });
     }
 
 }
 
+// login
 export const login = async (req, res) => {
     try {
-        const { email, password } = req.body;
+        const { email, password } = userLoginValidation.parse(req.body);
 
         const user = await pool.query("select * from users where email=$1", [email]);
         if (user.rows.length === 0) {
@@ -52,12 +70,22 @@ export const login = async (req, res) => {
                 message: "invalid credentials"
             });
         }
+
         const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, { expiresIn: "1h" })
         res.status(200).json({
-            message: "login successfully", token
-        })
+            message: "login successfully",
+            user: { id: user.rows[0].id, username: user.rows[0].username },
+            token,
+        });
     } catch (error) {
+        if (error instanceof ZodError) {
+            res.status(400).json({
+                message: "validation failed",
+                errors: error.errors,
+            });
+        };
         res.status(500).json({
+            message:"internal server error",
             error: error.message
         })
     }
