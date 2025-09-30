906b4a8400236b6ed2b4a07a58ec7d25bb78862a
monishkanigam <monishkanigam50@gmail.com>
2025-09-30 18:55:38 +0530

    complete backend api

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./src/routes/authRoutes.js"
 import { ConnectDB } from "./src/config/db.js";
+import cors from "cors";
 
 
 const app=express();
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    methods:["GET","POST","PUT","PATCH","DELETE"],
+    Credential:true,
+}))
 dotenv.config();
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-
 const PORT=process.env.PORT;
-
 
 ConnectDB();
 
