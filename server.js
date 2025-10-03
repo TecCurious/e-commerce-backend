6bfab7d9b5a1cb6688bc89cb6add43156b686a96
monishkanigam <monishkanigam50@gmail.com>
2025-10-03 21:56:53 +0530

    add .env package.json server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./src/routes/authRoutes.js"
@@ -5,11 +6,22 @@ import { ConnectDB } from "./src/config/db.js";
 import cors from "cors";
 
 
+=======
+import express from 'express';
+import dotenv from 'dotenv';
+import {ConnectDB} from './config/db.js'
+import authRouter from './routes/authRoute.js';
+import productsRoute from './routes/productRoute.js';
+import cors from 'cors';
+
+dotenv.config();
+>>>>>>> eb40ef4 (first commit)
 const app=express();
 
 app.use(cors({
     origin:"http://localhost:5173",
     methods:["GET","POST","PUT","PATCH","DELETE"],
+<<<<<<< HEAD
     Credential:true,
 }))
 dotenv.config();
@@ -32,3 +44,25 @@ app.get("/",(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`app is running on the port ${PORT}....`);
 });
+=======
+    credentials:true,
+}));
+
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const Port=process.env.PORT;
+ConnectDB();
+app.use('/api/auth',authRouter);
+app.use("/api/products",productsRoute);
+
+app.get("/",(req,res)=>{
+    res.send("welcome homepage");
+});
+
+app.listen(Port,()=>{
+    console.log(`server is running on port ${Port}`);
+})
+>>>>>>> eb40ef4 (first commit)
